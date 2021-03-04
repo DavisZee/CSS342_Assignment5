fb8405a91e160c3244e98677be4a0dd2a98976c8
Davis Zhong <zhongdavis2001@gmail.com>
2021-03-04 00:49:43 -0800

    Worked on main

--- main.cpp
+++ main.cpp
@@ -8,6 +8,7 @@
 // should test for duplicates because duplicates not allowed
 #include <iostream>
 #include "BSTree.h"
+#include <cassert>
 using namespace std;
 
 void test1() {
@@ -15,11 +16,23 @@ void test1() {
 }
 
 void test2() {
-    
+    cout << boolalpha; // prints boolean values as true/false rather than 1/0
+    BSTree myTree; // tree object creation
+    //
+    //test tree by adding in numbers 1 through 10 
+    //
+    int num(10);
+    for (int i = 0; i < num; i++) { // loops 10 times
+        assert(myTree.add(i)); // if fail to add then fail assert
+    }
+    cout << myTree << endl;
+
 }
 
 int main() {
-    test1();
-    
+    cout << "Begin Testing!" << endl << endl;
+    // test1();
+    int a = 5;
+    cout << "Done!" << endl;
     return 0;
 }
