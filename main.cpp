a8a7060a2614b7d7d735ef8b73deb86bb029778d
Davis Zhong <zhongdavis2001@gmail.com>
2021-03-14 04:20:45 -0700

    added comments in main

--- main.cpp
+++ main.cpp
@@ -11,13 +11,11 @@
 using namespace std;
 
 
-// Purpose: 
-// Preconditon:
-// Postcondition: 
-void test1() {
-    cout << "Hello" << endl << endl;
-}
 
+// Purpose: Tests basic constructors of the BSTree class.
+// Preconditon: Requires no arguments, assumes the class is included
+//              and the class constructors are available.
+// Postcondition: Constructors ran, success message printed.
 void testConstructors(){
     //works
     BSTree defaultTree();
@@ -27,11 +25,11 @@ void testConstructors(){
     BSTree copyTree(arrayTree);
     cout << "Test Constructors Passed" << endl << endl;
     
-}
+} // end of testConstructors
 
-// Purpose: 
-// Preconditon:
-// Postcondition: 
+// Purpose: Tests the success of the add methods of the BSTree class.
+// Preconditon: No arguments required.
+// Postcondition: all assertions pass, success message printed.
 void testAdd() {
     BSTree BST1(5);
     assert(BST1.add(12));   //Test adding
@@ -39,17 +37,18 @@ void testAdd() {
     assert(BST1.add(11));   //Test linking to newly added number
     assert(BST1.add(-12));  //Test negative number
 
+    // Making sure duplicates are not allowed
     assert(!BST1.add(12));
     assert(!BST1.add(-12));
 
     //Make sure to validate the construction of the tree with traversals
     //Make sure to use findNode to test if the threads work
     cout << "Test Add Passed" << endl << endl;
-}
+} // end of testAdd
 
-// Purpose: 
-// Preconditon:
-// Postcondition: 
+// Purpose: To test the contains and remove methods.
+// Preconditon: Requires no arguments.
+// Postcondition: All assertions pass, success message printed.
 void testContains() {
     BSTree BST1(10);
     assert(BST1.contains(3));   //Test contains and return false
@@ -69,11 +68,11 @@ void testContains() {
     cout << "Test Contains Passed" << endl << endl;
 
     //Add tests for contains after removal
-}
+} // end of testContains
 
-// Purpose: 
-// Preconditon:
-// Postcondition: 
+// Purpose: To test the remove method of the BSTree class.
+// Preconditon: No arguments required.
+// Postcondition: All assertions pass, success message printed.
 void testRemove() {
     BSTree BST1(10);
     assert(BST1.remove(2));        //Test remove
@@ -85,50 +84,52 @@ void testRemove() {
     assert(BST1.remove(13));       //Remove recently added value
 
     cout << "Test Remove Passed" << endl << endl;
-}
+} // end of testRemove
 
-// Purpose: 
-// Preconditon:
-// Postcondition: 
+// Purpose: To test the traversal method of the BSTree class
+// Preconditon: No arguments required.
+// Postcondition: All assertions pass, success message printed.
 void testTraversal() {
     
     BSTree BST1(5);                             
     string str = BST1.inorderTrav();                        
-    assert(str == "1 2 3 4 5 ");                            //Test inorderTraversal
+    assert(str == "1 2 3 4 5 ");            //Test inorderTraversal
     assert(BST1.add(23));
     assert(BST1.add(-1));
     str = BST1.inorderTrav();
-    assert(str == "-1 1 2 3 4 5 23 ");                      //Test traversal after addition
+    assert(str == "-1 1 2 3 4 5 23 ");      //Test traversal after addition
     assert(!BST1.add(1));
     str = BST1.inorderTrav();
-    assert(str == "-1 1 2 3 4 5 23 ");                      //Test traversal after failed addition
+    assert(str == "-1 1 2 3 4 5 23 ");      //Test traversal after failed addition
 
     assert(BST1.remove(3));
     str = BST1.inorderTrav();
-    assert(str == "-1 1 2 4 5 23 ");                        //Test traversal after deletion
+    assert(str == "-1 1 2 4 5 23 ");        //Test traversal after deletion
     assert(BST1.remove(2));
     str = BST1.inorderTrav();
-    assert(str == "-1 1 4 5 23 ");                          //Test traversal after second deletion
+    assert(str == "-1 1 4 5 23 ");          //Test traversal after second deletion
     assert(!BST1.remove(2));
     str = BST1.inorderTrav();
-    assert(str == "-1 1 4 5 23 ");                          //Test traversal after failed deletion
+    assert(str == "-1 1 4 5 23 ");          //Test traversal after failed deletion
 
     assert(BST1.add(45));                 
     assert(BST1.add(3));
     assert(BST1.add(2));
     str = BST1.inorderTrav();
-    assert(str == "-1 1 2 3 4 5 23 45 ");                   //Test traversal after multiple addition and deletion
+    assert(str == "-1 1 2 3 4 5 23 45 ");   //Test traversal after multiple addition and deletion
     
     BSTree copyTree(BST1);
     str = copyTree.inorderTrav();
-    assert(str == "-1 1 2 3 4 5 23 45 ");                   //Test traversal on copy tree
+    assert(str == "-1 1 2 3 4 5 23 45 ");   //Test traversal on copy tree
 
-    cout << "Test Traversal Passed" << endl << endl;
-}
+    cout << "Test Traversal Passed" << 
+    endl << endl;
+} // end of test traversal
 
-// Purpose: 
-// Preconditon:
-// Postcondition: 
+// Purpose: To run tests that are required in the assignment. Create a threaded
+//          binary tree of a size n then remove all even numbers from the tree.
+// Preconditon: No arguments required.
+// Postcondition: assertion passes, success message ran.
 void assignmentTest() {
     //Create tree with 20 nodes
     int size = 40;
@@ -143,9 +144,9 @@ void assignmentTest() {
     cout << "Assignment Test Passed" << endl << endl;
 }
 
-// Purpose: 
-// Preconditon:
-// Postcondition: 
+// Purpose: To test the outstream operator overload of BSTree class.
+// Preconditon: No arguments required.
+// Postcondition: Success fully prints out the tree in order.
 void testOutStream() {
     BSTree BST1(8); 
     //
@@ -156,9 +157,10 @@ void testOutStream() {
     cout << "Ostream Test Passed" << endl << endl;
 }
 
-// Purpose: 
-// Preconditon:
-// Postcondition: 
+// Purpose: Main method.
+// Preconditon: No arguments required.
+// Postcondition: All tests that are called passes, successful execution
+//                message printed.
 int main() {
     cout << "Begin Testing!" << endl << endl;
     // test1();
@@ -174,4 +176,4 @@ int main() {
     assignmentTest();
     cout << "Done!" << endl;
     return 0;
-}
+} // end of main
