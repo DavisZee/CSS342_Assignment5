e153babcc8548619047f78b0529eb576895fbf0a
Davis Zhong <zhongdavis2001@gmail.com>
2021-03-14 19:51:20 -0700

    Edits to comments in main

--- main.cpp
+++ main.cpp
@@ -17,11 +17,11 @@ using namespace std;
 //              and the class constructors are available.
 // Postcondition: Constructors ran, success message printed.
 void testConstructors(){
-    //works
+    // works
     BSTree defaultTree();
-    //works
+    // works
     BSTree arrayTree(10);
-    //works
+    // works
     BSTree copyTree(arrayTree);
     cout << "Test Constructors Passed" << endl << endl;
     
@@ -32,17 +32,17 @@ void testConstructors(){
 // Postcondition: all assertions pass, success message printed.
 void testAdd() {
     BSTree BST1(5);
-    assert(BST1.add(12));   //Test adding
-    assert(BST1.add(25));   //Test linking to newly added number
-    assert(BST1.add(11));   //Test linking to newly added number
-    assert(BST1.add(-12));  //Test negative number
+    assert(BST1.add(12));   // Test adding
+    assert(BST1.add(25));   // Test linking to newly added number
+    assert(BST1.add(11));   // Test linking to newly added number
+    assert(BST1.add(-12));  // Test negative number
 
     // Making sure duplicates are not allowed
     assert(!BST1.add(12));
     assert(!BST1.add(-12));
 
-    //Make sure to validate the construction of the tree with traversals
-    //Make sure to use findNode to test if the threads work
+    // Make sure to validate the construction of the tree with traversals
+    // Make sure to use findNode to test if the threads work
     cout << "Test Add Passed" << endl << endl;
 } // end of testAdd
 
@@ -51,23 +51,23 @@ void testAdd() {
 // Postcondition: All assertions pass, success message printed.
 void testContains() {
     BSTree BST1(10);
-    assert(BST1.contains(3));   //Test contains and return false
-    assert(!BST1.contains(12)); //Test contains and return false
-    assert(!BST1.contains(0));  //Test avoiding dummy node
+    assert(BST1.contains(3));   // Test contains and return false
+    assert(!BST1.contains(12)); // Test contains and return false
+    assert(!BST1.contains(0));  // Test avoiding dummy node
 
-    assert(BST1.add(14));       //Add new node 
-    assert(BST1.contains(14));  //Test contains on new node
-    assert(BST1.add(-122));     //Add new negative node
-    assert(BST1.contains(-122));//Test contains on new negative node]
-    assert(BST1.add(12));       //Add new node in between
-    assert(BST1.contains(12));  //Test contains on new node that is not largest
+    assert(BST1.add(14));       // Add new node 
+    assert(BST1.contains(14));  // Test contains on new node
+    assert(BST1.add(-122));     // Add new negative node
+    assert(BST1.contains(-122));// Test contains on new negative node]
+    assert(BST1.add(12));       // Add new node in between
+    assert(BST1.contains(12));  // Test contains on new node that is not largest
 
-    assert(BST1.remove(12));    //Remove a value
-    assert(!BST1.remove(12));   //check contains on removed value
+    assert(BST1.remove(12));    // Remove a value
+    assert(!BST1.remove(12));   // check contains on removed value
 
     cout << "Test Contains Passed" << endl << endl;
 
-    //Add tests for contains after removal
+    // Add tests for contains after removal
 } // end of testContains
 
 // Purpose: To test the remove method of the BSTree class.
@@ -75,13 +75,13 @@ void testContains() {
 // Postcondition: All assertions pass, success message printed.
 void testRemove() {
     BSTree BST1(10);
-    assert(BST1.remove(2));        //Test remove
-    assert(!BST1.remove(2));       //Test duplicate remove
-    assert(BST1.add(2));           //Add value again
-    assert(BST1.remove(2));        //Test added value
-    assert(!BST1.remove(123));     //Test non-existent value
-    assert(BST1.add(13));          //Add new value
-    assert(BST1.remove(13));       //Remove recently added value
+    assert(BST1.remove(2));        // Test remove
+    assert(!BST1.remove(2));       // Test duplicate remove
+    assert(BST1.add(2));           // Add value again
+    assert(BST1.remove(2));        // Test added value
+    assert(!BST1.remove(123));     // Test non-existent value
+    assert(BST1.add(13));          // Add new value
+    assert(BST1.remove(13));       // Remove recently added value
 
     cout << "Test Remove Passed" << endl << endl;
 } // end of testRemove
@@ -93,34 +93,34 @@ void testTraversal() {
     
     BSTree BST1(5);                             
     string str = BST1.inorderTrav();                        
-    assert(str == "1 2 3 4 5 ");            //Test inorderTraversal
+    assert(str == "1 2 3 4 5 ");            // Test inorderTraversal
     assert(BST1.add(23));
     assert(BST1.add(-1));
     str = BST1.inorderTrav();
-    assert(str == "-1 1 2 3 4 5 23 ");      //Test traversal after addition
+    assert(str == "-1 1 2 3 4 5 23 ");      // Test traversal after addition
     assert(!BST1.add(1));
     str = BST1.inorderTrav();
-    assert(str == "-1 1 2 3 4 5 23 ");      //Test traversal after failed addition
+    assert(str == "-1 1 2 3 4 5 23 ");      // Test traversal after failed addition
 
     assert(BST1.remove(3));
     str = BST1.inorderTrav();
-    assert(str == "-1 1 2 4 5 23 ");        //Test traversal after deletion
+    assert(str == "-1 1 2 4 5 23 ");        // Test traversal after deletion
     assert(BST1.remove(2));
     str = BST1.inorderTrav();
-    assert(str == "-1 1 4 5 23 ");          //Test traversal after second deletion
+    assert(str == "-1 1 4 5 23 ");          // Test traversal after second deletion
     assert(!BST1.remove(2));
     str = BST1.inorderTrav();
-    assert(str == "-1 1 4 5 23 ");          //Test traversal after failed deletion
+    assert(str == "-1 1 4 5 23 ");          // Test traversal after failed deletion
 
     assert(BST1.add(45));                 
     assert(BST1.add(3));
     assert(BST1.add(2));
     str = BST1.inorderTrav();
-    assert(str == "-1 1 2 3 4 5 23 45 ");   //Test traversal after multiple addition and deletion
+    assert(str == "-1 1 2 3 4 5 23 45 ");   // Test traversal after multiple addition and deletion
     
     BSTree copyTree(BST1);
     str = copyTree.inorderTrav();
-    assert(str == "-1 1 2 3 4 5 23 45 ");   //Test traversal on copy tree
+    assert(str == "-1 1 2 3 4 5 23 45 ");   // Test traversal on copy tree
 
     cout << "Test Traversal Passed" << 
     endl << endl;
